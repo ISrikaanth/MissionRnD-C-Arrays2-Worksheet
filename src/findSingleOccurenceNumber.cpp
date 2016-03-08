cdc59a361954e3c0e99e95e31a8bb9b43dcfaf12
ISrikaanth <ISrikaanth@missionrnd.com>
2016-03-08 23:28:56 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -14,14 +14,28 @@ NOTES:
 There are better ways of solving the problem than a brute-force solution which is of O(n^2)
 complexity .
 */
-
+#include <stdlib.h>
 int findSingleOccurenceNumber(int *A, int len)
 {
-	int i,pos;
+	if ((A == NULL) || (len <= 0))
+		return -1;
+	int i, max;
+	max = A[0];
 	for (i = 0; i < len; i++)
 	{
-		pos = A[i];
-
+		if (A[i]>max)
+			max = A[i];
 	}
-	return -1;
+	int *a;
+	a = (int *)malloc(max*sizeof(int));
+	for (i = 0; i < max; i++)
+		a[i] = 0;
+	for (i = 0; i < len; i++)
+		a[A[i]] = a[A[i]] + 1;
+	for (i = 0; i < max; i++)
+	{
+		if (a[i] == 1)
+			break;
+	}
+	return i;
 }
